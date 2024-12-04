b7e8908ac67340048e35e1b203870fbfe10a8ca4
sashaalexwhite <121976355+sashaalexwhite@users.noreply.github.com>
2024-12-04 18:59:23 +0200

    Update add_n_m_file.py

--- modules/add_n_m_file.py
+++ modules/add_n_m_file.py
@@ -4,7 +4,6 @@ import os
 
 from dags.session_prediction_v2_main import add_n_m_file_task
 
-# Настройка логирования
 logging.basicConfig(level=logging.INFO,
                     format='%(asctime)s - %(levelname)s - %(message)s',
                     filename='adding_to_the_db.log',
@@ -12,42 +11,34 @@ logging.basicConfig(level=logging.INFO,
 
 path = os.environ.get("PROJECT_PATH", ".")
 
-# Установка пути к файлу учетных данных
 os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = f"{path}/credentials/deep-ethos-436810-c7-32b7bed8002f.json"
 
-# Инициализация клиентов Google Cloud Storage и BigQuery
+
 storage_client = storage.Client()
 bigquery_client = bigquery.Client()
 
-# Название вашего ведра и таблицы BigQuery
 bucket_name = 'basa_data_diplom'
 dataset_id = 'my_dataset'
 table_id = 'diplom_bd'
 bucket = storage_client.bucket(bucket_name)
 
-# Путь к локальной папке с обработанными файлами
 local_folder = f'{path}/data/predictions'
 
 def add_n_m_file():
-    logging.info('Начало выполнения функции add_n_m_file')
-    # Загрузка каждого файла из локальной папки в ведро и добавление данных в BigQuery
+    logging.info('Starting a Function add_n_m_file')
     for filename in os.listdir(local_folder):
-        if filename.endswith('.parquet'):  # Предполагается, что файлы в формате Parquet
+        if filename.endswith('.parquet'):  
             local_file_path = os.path.join(local_folder, filename)
             blob = bucket.blob(filename)
             try:
-                # Загрузка файла в ведро
                 blob.upload_from_filename(local_file_path)
-
-                # Загрузка данных из файла во временную таблицу в BigQuery
                 temp_table_id = f"{table_id}_temp"
                 table_ref = bigquery_client.dataset(dataset_id).table(temp_table_id)
                 job_config = bigquery.LoadJobConfig(source_format=bigquery.SourceFormat.PARQUET)
                 with open(local_file_path, "rb") as source_file:
                     load_job = bigquery_client.load_table_from_file(source_file, table_ref, job_config=job_config)
-                load_job.result()  # Ожидание завершения загрузки
+                load_job.result()  
 
-                # Вставка новых данных в основную таблицу
                 query = f"""
                     INSERT INTO `{dataset_id}.{table_id}`
                     SELECT
@@ -76,19 +67,16 @@ def add_n_m_file():
                     GROUP BY session_id
                 """
                 query_job = bigquery_client.query(query)
-                query_job.result()  # Ожидание завершения запроса
+                query_job.result()  
 
-                # Удаление временной таблицы
                 bigquery_client.delete_table(table_ref)
-
-                # Удаление локального файла
                 os.remove(local_file_path)
 
             except Exception as e:
                 os.remove(local_file_path)
-                logging.error(f'Ошибка при обработке файла {filename}: {e}')
+                logging.error(f'Error processing the file {filename}: {e}')
 
-    logging.info("Все файлы успешно загружены в Google Cloud Storage и BigQuery.Локальная папка стёрта ")
+    logging.info("All files have been successfully uploaded to the Google Cloud Storage and BigQuery.Локальная папка стёрта ")
 
 if __name__ == '__main__':
     add_n_m_file()
